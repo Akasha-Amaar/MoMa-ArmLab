5713ac87a0a695cf65e19a9224481227a8fd3a94
Akasha-Amaar <akashaamaar786@gmail.com>
2026-06-10 11:43:40 +0200

    Add launch file for communication nodes

--- ros2_ws/src/arm_lab/setup.py
+++ ros2_ws/src/arm_lab/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'arm_lab'
 
@@ -10,7 +12,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-    ],
+        ('share/' + package_name + '/launch',
+    	glob('launch/*.launch.py')),
+    	],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='akasha',
